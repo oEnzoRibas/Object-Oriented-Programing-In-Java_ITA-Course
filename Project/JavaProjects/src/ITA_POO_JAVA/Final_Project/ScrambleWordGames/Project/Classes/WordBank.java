4ebd55577da4718be6f006293c53fcc120c4da10
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-03-15 21:26:16 -0300

    a

--- Project/JavaProjects/src/ITA_POO_JAVA/Final_Project/ScrambleWordGames/Project/Classes/WordBank.java
+++ Project/JavaProjects/src/ITA_POO_JAVA/Final_Project/ScrambleWordGames/Project/Classes/WordBank.java
@@ -1,4 +1,13 @@
 package ITA_POO_JAVA.Final_Project.ScrambleWordGames.Project.Classes;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class WordBank {
+    public static void main(String[] args) {
+
+        Scanner scannerIn = new Scanner(System.in);
+
+        HashMap<>
+    }
 }
