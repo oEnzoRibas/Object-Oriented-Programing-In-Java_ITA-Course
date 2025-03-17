99a5701043fb68fb19f14796497b91f389ad9752
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-03-17 16:24:06 -0300

    creating interface scrambler

--- Project/JavaProjects/src/ITA_POO_JAVA/Final_Project/ScrambleWordGames/Project/Classes/ScramblerFactory.java
+++ Project/JavaProjects/src/ITA_POO_JAVA/Final_Project/ScrambleWordGames/Project/Classes/ScramblerFactory.java
@@ -1,4 +1,26 @@
 package ITA_POO_JAVA.Final_Project.ScrambleWordGames.Project.Classes;
 
-public class ScramblerFactory {
+import ITA_POO_JAVA.Final_Project.ScrambleWordGames.Project.Interfaces.Scrambler;
+
+public class ScramblerFactory implements Scrambler {
+
+    @Override
+    public String easy(String word) {
+        return "";
+    }
+
+    @Override
+    public String normal(String word) {
+        return "";
+    }
+
+    @Override
+    public String hard(String word) {
+        return "";
+    }
+
+    @Override
+    public String reverse(String word) {
+        return "";
+    }
 }
--- Project/JavaProjects/src/ITA_POO_JAVA/Final_Project/ScrambleWordGames/Project/Interfaces/Scrambler.java
+++ Project/JavaProjects/src/ITA_POO_JAVA/Final_Project/ScrambleWordGames/Project/Interfaces/Scrambler.java
@@ -1,4 +1,8 @@
 package ITA_POO_JAVA.Final_Project.ScrambleWordGames.Project.Interfaces;
 
 public interface Scrambler {
+    public String easy(String word);
+    public String normal(String word);
+    public String hard(String word);
+    public String reverse(String word);
 }
