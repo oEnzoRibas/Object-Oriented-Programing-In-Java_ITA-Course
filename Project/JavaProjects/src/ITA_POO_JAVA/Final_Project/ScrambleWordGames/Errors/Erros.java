a2a3a15a8021ceb934d29ceea3bf2fa00a541b8a
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-03-17 13:00:08 -0300

    a

--- Project/JavaProjects/src/ITA_POO_JAVA/Final_Project/ScrambleWordGames/Errors/Erros.java
+++ Project/JavaProjects/src/ITA_POO_JAVA/Final_Project/ScrambleWordGames/Errors/Erros.java
@@ -3,7 +3,7 @@ package ITA_POO_JAVA.Final_Project.ScrambleWordGames.Errors;
 public class Erros {
     public static void printERROR(Exception e){
         System.out.printf("""
-                    "ERROR!!! File Not Found!"
+                    "ERROR!!!"
                     Cause: %s
                     Message: %s
                     """,e.getCause(),e.getMessage());
