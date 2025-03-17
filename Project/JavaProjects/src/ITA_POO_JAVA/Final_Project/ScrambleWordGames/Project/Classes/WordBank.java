7fa2a307902cf49e31a233a692e8722a2ef00cc7
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-03-17 16:14:03 -0300

    implementing constructor and returnword method update on wordbank

--- Project/JavaProjects/src/ITA_POO_JAVA/Final_Project/ScrambleWordGames/Project/Classes/WordBank.java
+++ Project/JavaProjects/src/ITA_POO_JAVA/Final_Project/ScrambleWordGames/Project/Classes/WordBank.java
@@ -5,35 +5,33 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
+
 import static ITA_POO_JAVA.Final_Project.ScrambleWordGames.Exceptions.Erros.printERROR;
 
 public class WordBank {
+    File wordFilePath = new File("Final_Project/ScrambleWordGames/words.txt");
+    Scanner file = null;
+    int k = 0;
+    Random r = new Random();
+    int key;
+    HashMap<Integer, String> wordsHMap = new HashMap<>();
 
-    public static String returnWord() {
-        File wordFile = new File("Final_Project/ScrambleWordGames/words.txt");
-        Scanner file = null;
-        int k = 0;
-        Random r = new Random();
-        int key;
-        HashMap<Integer, String> wordsHMap = new HashMap<>();
+    public WordBank() {
         try {
-            file = new Scanner(wordFile);
-        } catch (FileNotFoundException e) {
-            printERROR(e);
-        }
-        try {
-            while (file.hasNextLine()) {
+            this.file = new Scanner(this.wordFilePath);
+            while (this.file.hasNextLine()) {
                 String word = file.nextLine();
-                wordsHMap.put(k, word);
-                k++;
+                this.wordsHMap.put(this.k, word);
+                this.k++;
             }
-        } catch (NullPointerException e) {
-            printERROR(e);
-        }
-        key = r.nextInt(wordsHMap.size());
+        } catch (Exception e) {printERROR(e);}
+    }
+
+    public String returnWord() {
+        this.key = this.r.nextInt(this.wordsHMap.size());
         // wordsHMap.forEach((key, val) -> System.out.printf("%s = %s%n", key, val));
-       return wordsHMap.get((key));
+        return this.wordsHMap.get((this.key));
     }
-    }
+}
 
 
--- Project/JavaProjects/src/ITA_POO_JAVA/Final_Project/ScrambleWordGames/Project/Main.java
+++ Project/JavaProjects/src/ITA_POO_JAVA/Final_Project/ScrambleWordGames/Project/Main.java
@@ -4,5 +4,8 @@ import ITA_POO_JAVA.Final_Project.ScrambleWordGames.Project.Classes.WordBank;
 
 public class Main {
     public static void main(String[] args) {
+        WordBank wb = new WordBank();
+
+        System.out.println(wb.returnWord());
     }
 }
